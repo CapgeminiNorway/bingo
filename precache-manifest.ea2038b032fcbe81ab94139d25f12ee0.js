50c41e67400f17dc6eb9500f331bc0dc0bec0b62
Stian Thaulow <stian.thaulow@capgemini.com>
2020-03-28 12:48:38 +0100

    Updates

---
+++ precache-manifest.ea2038b032fcbe81ab94139d25f12ee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66dca722946aa4e9ff378331afa7184c",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "8d5161e23a5e9e006984",
+    "url": "/bingo/static/css/main.5acca473.chunk.css"
+  },
+  {
+    "revision": "c1276fcdf335843a8124",
+    "url": "/bingo/static/js/2.c2aaa7d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bingo/static/js/2.c2aaa7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5161e23a5e9e006984",
+    "url": "/bingo/static/js/main.27722515.chunk.js"
+  },
+  {
+    "revision": "3e96d5cca2553c021ce2",
+    "url": "/bingo/static/js/runtime-main.d558581a.js"
+  }
+]);
